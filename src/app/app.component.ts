aa3ef1eb02725d23bbd70afec6cb23253bc37f79
Vinícius <vinileardini@gmail.com>
2024-11-20 11:19:59 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header/header.component';
+import { CardComponent } from './card/card/card.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nome-do-projeto';
+  title = 'plants';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './card/card/card.component';
+import { BigCardComponent } from './big-card/big-card/big-card.component';
+import { HomepageComponent } from './home/homepage/homepage.component';
+export const routes: Routes = [
+  {
+    path:'',
+    component:HomepageComponent
+  },
+  {
+    path:'content/:id',
+    component:BigCardComponent
+  }
+];
 
-export const routes: Routes = [];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
